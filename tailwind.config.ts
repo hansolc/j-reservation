5620db149e43c082792ee3393c8140300a45140e
hansolc <hsc080808@naver.com>
2024-10-21 02:13:27 +0900

    add primary, kakao for custom color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#007AFF",
+        kakao: "#F9E000",
       },
     },
   },
