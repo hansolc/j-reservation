ef908a901bcd5d5e4c0e72f32f28083ed5c65eab
hansolc <hsc080808@naver.com>
2024-10-29 04:21:39 +0900

    add myFetch func which like axios

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,4 @@
+import { MyFetchProps } from "@/types/api";
 import { Dispatch, SetStateAction } from "react";
 
 const handleApiCall = async <T>(
@@ -9,17 +10,18 @@ const handleApiCall = async <T>(
   setError(null);
   try {
     const res = await apiCall();
-    if (!res.ok) {
+
+    if (res.ok) {
+      const data = (await res.json()) as T;
+      return data;
+    } else {
       const errJson = await res.json();
       throw {
         status: res.status,
         statusText: res.statusText,
-        message: errJson,
+        message: errJson || "unkonwn error",
       };
     }
-
-    const data = (await res.json()) as T;
-    return data;
   } catch (error) {
     if (error instanceof Error) {
       console.error(error);
@@ -35,4 +37,30 @@ const handleApiCall = async <T>(
   }
 };
 
-export default handleApiCall;
+const myFetch = {
+  get: async ({ url, token }: MyFetchProps) =>
+    await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }),
+  post: async ({ url, token, body }: MyFetchProps) =>
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    }),
+  delete: async ({ url, token }: MyFetchProps) =>
+    await fetch(url, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }),
+};
+
+export { handleApiCall, myFetch };
