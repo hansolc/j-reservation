ecfd73419475c01f725ea6d6af01f03aeee8a8e3
hansolc <hsc080808@naver.com>
2024-11-18 13:12:52 +0900

    temporal fixed for button position in use registration page

--- src/components/user/registration/RegistrationManager.tsx
+++ src/components/user/registration/RegistrationManager.tsx
@@ -32,29 +32,33 @@ const RegistrationManager = ({ type }: { type: "regis" | "login" }) => {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="relative h-full">
         <div className="px-5">
           <LoginForm info={info} handleChange={handleInputChange} />
         </div>
-        {type === "regis" ? (
-          <div className="text-center text-sm">
-            <p className="">이미 아이디가 있으신 가요?</p>
-            <p
-              className="text-[#575757] border-b font-bold inline cursor-pointer"
-              onClick={() => router.push("/registration/login")}
-            >
-              로그인 하기
-            </p>
-          </div>
-        ) : null}
-        <div>
-          {loading ? (
-            "...loading"
-          ) : (
-            <Button color="primary" size="lg" isRadius type="submit">
-              {type === "regis" ? "회원가입" : "로그인"}하기
-            </Button>
+        <div className="absolute bottom-10 w-full">
+          {type === "regis" && (
+            <div className="pb-5">
+              <div className="text-center text-sm">
+                <p className="">이미 아이디가 있으신 가요?</p>
+                <p
+                  className="text-[#575757] border-b font-bold inline cursor-pointer"
+                  onClick={() => router.push("/registration/login")}
+                >
+                  로그인 하기
+                </p>
+              </div>
+            </div>
           )}
+          <div>
+            {loading ? (
+              "...loading"
+            ) : (
+              <Button color="primary" size="lg" isRadius type="submit">
+                {type === "regis" ? "회원가입" : "로그인"}하기
+              </Button>
+            )}
+          </div>
         </div>
       </form>
     </>
